b1021c31443abb713b1c2de4207bdcb7ea12dd91
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-16 15:46:17 +0300

    123

---
+++ Task1/LinEquation.cs
@@ -0,0 +1,29 @@
+﻿namespace Task1
+{
+     struct LinEquation
+    {
+        double k;
+        double b;
+
+        public LinEquation (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+
+            if (k==0 && b!=0)
+            {
+                return $"решений нет";
+            }
+            else if (k == 0 && b == 0)
+            {
+                return $"решений бесконечно много, x -- любое число";
+            }
+            else
+            return $"x={(double)(-b / k)}";
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace Task1;
+//Разработать структуру для решения линейного уравнения 0=kx+b.
+//Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+//Для решения уравнения предусмотреть метод Root.
+//Создать экземпляр разработанной структуры.
+//Осуществить использование экземпляра в программе.
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите k");
+        double k = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите b");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        LinEquation eq = new LinEquation(k,b);
+        Console.WriteLine(eq.Root());
+        Console.ReadKey();
     }
 }
+
+
 
